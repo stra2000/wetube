7b45b3ef0b1460af4a914dbd792ece26195685be
stra2000 <stra2003@naver.com>
2019-04-21 07:11:46 +0900

    WeTubeMongoDB

--- middleware.js
+++
@@ -1,7 +0,0 @@
-import routes from "./routes";
-
-export const localMiddleware = (req,res,next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    next();
-}
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,13 +1,29 @@
 import express from "express";
 import routes from "../routes";
-import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import {
+  getUpload,
+  postUpload,
+  videoDetail,
+  deleteVideo,
+  getEditVideo,
+  postEditVideo
+} from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+//Upload
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+//Video Detail
+videoRouter.get(routes.videoDetail(), videoDetail);
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
